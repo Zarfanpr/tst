5ac069a21428dcfc9ed5bb089f5a1ea13fbc86f7
Zarfanpr <zarfapratista@gmail.com>
2021-10-18 15:15:44 +0700

    coba 1

---
+++ mainauth.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    is_active: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+class UserCreate(User):
+    password: str
